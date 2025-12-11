f3e9c687ccdb65ae8584bcf53fc15b788c030f3b
Devv1507 <davide.valencia.v@gmail.com>
2025-12-11 14:05:03 -0500

    fix: updated component idnex

--- src/shared/components/index.js
+++ src/shared/components/index.js
@@ -1,3 +1,5 @@
 export { StatCard } from './StatCard';
 export { StationCard } from './StationCard';
 export { SensorCard } from './SensorCard';
+export { default as MetricsView } from './Dashboard/MetricsView';
+export { default as TrendsView } from './Dashboard/TrendsView';
